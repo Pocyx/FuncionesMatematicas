c0d8942ae95c7aa76564e765ab7fc9aa671b0f9b
Andres Galvez <pocyxDesigner@hotmail.com>
2017-01-17 21:25:40 +0100

    libreria array1

---
+++ funciones/Arrays1.java
@@ -0,0 +1,174 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package funciones;
+
+import static java.lang.Byte.MAX_VALUE;
+import static java.lang.Byte.MIN_VALUE;
+
+/**
+ *
+ * @author PocyxDesigner
+ */
+public class Arrays1 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        int[] y = {1,2,3,4,5};
+        System.out.print(mediaArrayInt(y));
+        //
+        int n =2;
+        System.out.println(rotaDerechaArrayInt(n,y));
+    }
+    
+    
+    /**
+     * 
+     * Genera un array de tamaño n con números aleatorios
+       cuyo intervalo (mínimo y máximo) se indica como parámetro.
+     * 
+     * @param n tamaño del array
+     * @param min intervalo aleatorio minimo
+     * @param max intervalo aleatorio minimo
+     * @return array
+     */
+    public static int[] generaArrayInt(int n, int min, int max){
+        int[] array = new int [n];
+        for(int i=0;i<array.length;i++){
+            array[i]= (int) (Math.random()*max)+min;
+        }
+        return array;
+    }
+    /**
+     * 
+     * Devuelve el mínimo del array que se pasa como
+       parámetro.
+     * 
+     * @param array
+     * @return 
+     */
+    public static int minimoArrayInt(int[] array){
+        int minimo = MAX_VALUE;
+        for(int i=0;i<array.length;i++){
+            
+            if(array[i]<minimo){
+                minimo=array[i];
+            }
+        }
+        return minimo;
+    }
+    /**
+     * 
+     * Devuelve el máximo del array que se pasa como
+       parámetro.
+     * 
+     * @param array
+     * @return 
+     */
+    public static int maximoArrayInt(int[] array){
+        int maximo = MIN_VALUE;
+        for(int i=0;i<array.length;i++){
+            
+            if(array[i]>maximo){
+                maximo=array[i];
+            }
+        }
+        return maximo;
+    }
+    /**
+     * 
+     * Devuelve la media del array que se pasa como parámetro.
+     * 
+     * @param x
+     * @return 
+     */
+    public static double mediaArrayInt(int[] x){
+        int suma=0;
+        for(int i=0;i< x.length;i++){
+            suma=x[i]+suma;
+        }
+        int media=suma/x.length;
+        return media;
+    }
+    /**
+     * 
+     * Dice si un número está o no dentro de un array.
+     * 
+     * @param n
+     * @param array
+     * @return 
+     */
+    public static boolean estaEnArray(int n, int[] array){
+        boolean esta = false;
+        
+        for(int i=0;i< array.length;i++){
+            if(n==array[i]){
+                esta=true;
+            }
+        }
+        return esta;
+    }
+    /**
+     * 
+     * Busca un número en un array y devuelve la posición
+       (el índice) en la que se encuentra.
+     * 
+     * @param n
+     * @param array
+     * @return si no esta el numero en el array devuelve 0.
+     */
+    public static int posicionEnArray(int n, int[] array){
+        int indice=0;
+        for(int i=0;i< array.length;i++){
+            if(n==array[i]){
+                indice=i+1;
+            }
+        }
+        return indice;
+    }
+    /**
+     * Le da la vuelta a un array.
+     * @param array
+     * @return 
+     */
+    public static int[] volteaArrayInt(int[] array){
+        int[] volteado=new int[array.length];
+        int e=array.length-1;
+        for(int i=0;i<array.length ;i++){  
+            volteado[e]=array[i];
+            e--;
+        }
+        return volteado;
+    }
+    /**
+     * 
+     * Rota n posiciones a la derecha los números de
+       un array.
+     * 
+     * @param n
+     * @param array
+     * @return 
+     */
+    public static int[] rotaDerechaArrayInt(int n, int[] array){
+        int[] rotado=new int[array.length];
+        int e = n;
+        int m = 0;
+        for(int i=0;i<array.length;i++){
+            if(i==0){
+                rotado[i] = array[array.length-n];
+                }else if (i<array.length-n-i){
+                    rotado[i] = array[array.length-n+i];
+                }else if (i>=n && i<array.length){
+                    rotado[i] = array[m++];
+                }else if(i==array.length-1){
+                    rotado[i] = array[e++];
+                }   
+        }                
+        return rotado;
+
+    }
+}
--- funciones/Funciones.java
+++ funciones/Funciones.java
@@ -15,6 +15,7 @@ public class Funciones {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
+        //Matematicas//
         System.out.println("esCapicua "+Matematicas.esCapicua(12321));
         System.out.println("esPimo "+Matematicas.esPrimo(5));
         System.out.println("siguiente primo "+Matematicas.siguientePrimo(5));
@@ -29,6 +30,33 @@ public class Funciones {
         System.out.println("pegarPorDelante "+Matematicas.pegaPorDelante(12345, 2));
         System.out.println("trozoNumero "+Matematicas.trozoDeNumero(123456, 2, 4));
         System.out.println("juntaNumeros "+Matematicas.juntaNumeros(12345, 24946));
+        //Arays1//
+        System.out.println();
+        System.out.println("Funciones de array\n");
+        int n = 5;//tamaño de array
+        int[] arrayUno = Arrays1.generaArrayInt(n, 1, 10);
+        System.out.print("generaArrayInt ");
+        for(int i=0;i<n;i++){
+            System.out.print(arrayUno[i]+", ");
+        }
+        System.out.println();
+        System.out.println("minimoArrayInt "+Arrays1.minimoArrayInt(arrayUno));
+        System.out.println("maximoArrayInt "+Arrays1.maximoArrayInt(arrayUno));
+        System.out.println("mediaArrayInt "+Arrays1.mediaArrayInt(arrayUno));
+        System.out.println("estaEnArray "+Arrays1.estaEnArray(2,arrayUno));
+        System.out.println("posicionEnArray "+Arrays1.posicionEnArray(2,arrayUno));
+        System.out.print("volteaArrayInt ");
+        int[] arrayDos = Arrays1.volteaArrayInt(arrayUno);
+        for(int i=0;i<n;i++){
+            System.out.print(arrayDos[i]+", ");
+        }
+        System.out.println();
+        System.out.print("rotaDerechaArrayInt ");
+        int[] arrayTres = Arrays1.rotaDerechaArrayInt(2, arrayDos);
+        for(int i=0;i<n;i++){
+            System.out.print(arrayTres[i]+", ");
+        }
+        System.out.println();
     }
     
 }
